c00eaaf53ddf22509ab0a8e9ed64c1f9a2d8359c
Shubham kumar Shukla <shubhamkumarshukla@Shubhams-MacBook-Air.local>
2026-06-04 10:13:01 +0530

    Add prisma client

--- app/college/[id]/page.tsx
+++ app/college/[id]/page.tsx
@@ -1,13 +1,5 @@
-async function getCollege(id: string) {
-  const res = await fetch(
-    `http://localhost:3000/api/colleges/${id}`,
-    {
-      cache: "no-store",
-    }
-  );
-
-  return res.json();
-}
+import prisma from "@/lib/prisma";
+import Link from "next/link";
 
 export default async function CollegeDetails({
   params,
@@ -16,12 +8,23 @@ export default async function CollegeDetails({
 }) {
   const { id } = await params;
 
-  const college = await getCollege(id);
+  const college = await prisma.college.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
+
+  if (!college) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-black text-white">
+        <h1 className="text-3xl font-bold">College Not Found</h1>
+      </div>
+    );
+  }
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-950 to-blue-950 text-white p-8">
       <div className="max-w-5xl mx-auto">
-
         <img
           src="https://images.unsplash.com/photo-1562774053-701939374585?w=1200"
           alt="College"
@@ -29,38 +32,34 @@ export default async function CollegeDetails({
         />
 
         <div className="bg-gray-900/70 p-8 rounded-3xl">
-
           <h1 className="text-5xl font-bold mb-6 text-blue-400">
             {college.name}
           </h1>
 
           <div className="grid md:grid-cols-3 gap-6 mb-8">
-
             <div className="bg-gray-800 p-5 rounded-2xl">
-              <p>📍 {college.location}</p>
+              <p className="text-lg">📍 {college.location}</p>
             </div>
 
             <div className="bg-gray-800 p-5 rounded-2xl">
-              <p>💰 ₹{college.fees}</p>
+              <p className="text-lg">💰 ₹{college.fees}</p>
             </div>
 
             <div className="bg-gray-800 p-5 rounded-2xl">
-              <p>⭐ {college.rating}</p>
+              <p className="text-lg">⭐ {college.rating}</p>
             </div>
-
           </div>
 
           <p className="text-gray-300 text-lg">
             {college.description}
           </p>
 
-          <a
+          <Link
             href="/"
-            className="inline-block mt-8 bg-blue-500 px-6 py-3 rounded-xl"
+            className="inline-block mt-8 bg-blue-500 hover:bg-blue-600 px-6 py-3 rounded-xl transition"
           >
             Back to Home
-          </a>
-
+          </Link>
         </div>
       </div>
     </div>
